54b03bf71037808f3d19f89c54f65ecb8c6b3e80
Jonathan98SM <Jonathan98SM>
2022-11-25 18:58:05 -0500

    upload corregido 2

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import multer from 'multer';
 const app = express();
 app.listen(PORT);
 app.set('view engine','ejs');
-app.set('views',path.join('/src','views'));
+app.set('views','./views');
 
 // Middlewares
 app.use(morgan('dev'));
