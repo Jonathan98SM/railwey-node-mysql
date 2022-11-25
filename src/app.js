11373b8b9ff317c3f366b7f3daa2036fb43fe7ac
Jonathan98SM <Jonathan98SM>
2022-11-25 17:55:32 -0500

    trabajnado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
-import express, { json } from 'express'
-import { PORT } from './config.js'
-import { pool } from './db.js' 
+import express from 'express';
+import path from 'path';
+import { PORT } from './config.js';
+import { pool } from './db.js' ;
+import morgan from 'morgan';
+import multer from 'multer';
 
-const app = express()
+const app = express();
 
-app.listen(PORT)
+app.listen(PORT);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+// Middlewares
+app.use(morgan('dev'));
+
+
 app.get('/',async (req,res)=>{
     const [row] =await pool.query('SELECT * FROM users')
     res.json(row)
