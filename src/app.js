346d7cbc17317d38a6c37932803fd49f9a9c1212
Jonathan98SM <Jonathan98SM>
2022-11-25 17:08:56 -0500

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from 'express'
+import { PORT } from './config.js'
+import { pool } from './db.js' 
+
+const app = express()
+
+app.listen(PORT)
+app.get('/',async (req,res)=>{
+    const [row] =await pool.query('SELECT * FROM users')
+    res.json(row)
+})
+app.get('/create',async (req,res)=>{
+    const result = await pool.query('INSERT INTO users(name) VALUES ("Jon")')
+    res.json(result)
+})
