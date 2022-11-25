8928f06bd03e9d97ab67fbbf2a9313e7e0361179
Jonathan98SM <Jonathan98SM>
2022-11-25 18:04:52 -0500

    trabajnado sin views

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.listen(PORT);
 app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+//app.set('views',path.join(__dirname,'views'));
 
 // Middlewares
 app.use(morgan('dev'));
